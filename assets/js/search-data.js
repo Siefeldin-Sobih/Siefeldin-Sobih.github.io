59cf0c7883cbac6f0c5ce9171f86cf8c86eaf7cb
Siefeldin-Sobih <siefeldinsobih@gmail.com>
2025-08-08 09:59:22 +0000

    Deploying to gh-pages from @ Siefeldin-Sobih/Siefeldin-Sobih.github.io@127d64e1168a13b32ffd866d2af1383dc9f02929 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,11 +10,11 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-resume",
-          title: "RESUME",
+          title: "Resume",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/resume/";
           },
         },{id: "nav-publications",
           title: "Publications",
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "Repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
